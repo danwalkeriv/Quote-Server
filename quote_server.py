d6e898a9f6ed7e793e383f40812a254731d0c4ae
Dan <dan@dan-netbook.(none)>
2011-11-30 00:20:06 -0700

    Small changes to documentation.

--- quote_server.py
+++ quote_server.py
@@ -8,6 +8,7 @@ quotes = None
 
 # File parsing
 def load_quotes(quote_filename):
+    """Parses a quote file that consists of quotes separated by empty lines."""
     quotes = []
     with open(quote_filename) as quote_file:
         current_quote = []
@@ -26,11 +27,14 @@ def load_quotes(quote_filename):
 # Routes
 @bottle.route('/')
 def random_quote():
+    """Returns a random quote from the global list of quotes."""
     global quotes
     return random.sample(quotes, 1)[0]
 
 
 def run_server(quote_filename):
+    """Starts the bottle server after loading the quotes from the specified
+    file."""
     global quotes
     quotes = load_quotes(quote_filename)
     bottle.run(host='localhost', port=8080)
@@ -45,6 +49,7 @@ def main(argv = None):
     parser.add_argument('--quote-file', default='christmas_messages.txt')
     options = parser.parse_args(argv)
     run_server(options.quote_file)    
+
 
 if __name__ == "__main__":
     sys.exit(main())
